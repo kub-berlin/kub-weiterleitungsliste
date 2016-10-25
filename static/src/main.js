e41ed697d5f71d75f63cae7b474e59e41d89b5bc
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-10-25 08:42:59 +0200

    revert feature-offline
    -	adds unnecessary complexity and maintenance load
    -	is hard to get right with HTTP Auth

--- static/src/main.js
+++ static/src/main.js
@@ -183,15 +183,6 @@ var onCategoryChange = function(event, state, app) {
 };
 
 
-// register service worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function(reg) {
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    console.log('Registration failed with ' + error);
-  });
-}
-
 // main
 var app = createApp(template);
 
